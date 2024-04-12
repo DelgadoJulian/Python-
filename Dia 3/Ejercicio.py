9fe14d653bca8964bd3fb3b9d62990ceaf22d8cb
DelgadoJulian <thelonelyman22@gmail.com>
2024-04-12 02:30:49 -0500

    Ejercicio

--- Dia 3/Ejercicio.py
+++ Dia 3/Ejercicio.py
@@ -5,17 +5,43 @@ print("/----------------------------------------------------/")
 print("/       Welcome, Estos son mis programas             /")
 print("/                                                    /")
 print("/ Selecione una accion a realizar:                   /")
-print("/ a) Programa para determinar un numero primo        /")
-print("/ b) Programa para probar contraseñas                /")
+print("/ 1) Programa para determinar un numero primo        /")
+print("/ 2) Programa para probar contraseñas                /")
 print("/----------------------------------------------------/")
-respuesta=str(input())
 
-print("")
+registri=(input())
 
-if respuesta == respuesta == a: 
+
+
+if registri == 1:
     print("Asi que quieres determinar un numero primo")
+    print("El proposito de este programa es saber si el numero ingresado es primo o no")
     print(" ")
     print("Por favor ingrese un numero para determinar si es primo")
     print(" ")
 
-numerito=int(input())
+    Num=int(input("Ingrese un numero: "))
+
+    if Num > 1: 
+        cont=0
+    for i in range (2, Num):
+        resto=Num%i
+        #print("{} & {} = {}".format(Num, i, resto))
+        if resto==0:
+            cont+=1
+    if cont==0: 
+        print("El {} es un numero primo".format(Num))
+    else: 
+        print("El numumero {} no es un numero primo".format(Num))
+else: 
+    print("El numumero {} no es un numero primo".format(Num))   
+
+
+
+
+import random
+
+minusculas= "abcdefghijklmnñopqrstuvwxyz"
+mayuscula= "ABCDEFHIJKLMNÑOPQRSTUVWXYZ"
+numeros= "0,1,2,3,4,5,6,7,8,9"
+Simbolos= ""
